876d0c50c90fcc131eb0fd21deb8763882b9d9ff
Imtiaz Ahmed <imtiaz.ahmed@edraaksystems.com>
2023-03-02 12:38:54 +0500

    first commit

---
+++ src/config/ConfigFirebase.js
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmq49Nw2OfeZZBh45edrWwGSXg3JbJt_g",
+  authDomain: "gymapp-98ff8.firebaseapp.com",
+  projectId: "gymapp-98ff8",
+  storageBucket: "gymapp-98ff8.appspot.com",
+  messagingSenderId: "527742926348",
+  appId: "1:527742926348:web:853bf1d4b575777946f829",
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+export default firebase;
+
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "REPLACE_HERE",
+//   authDomain: "REPLACE_HERE",
+//   projectId: "REPLACE_HERE",
+//   storageBucket: "REPLACE_HERE",
+//   messagingSenderId: "REPLACE_HERE",
+//   appId: "REPLACE_HERE"
+// };
+
+// // Initialize Firebase
+// const firebase = initializeApp(firebaseConfig);
+
+// export default firebase;
